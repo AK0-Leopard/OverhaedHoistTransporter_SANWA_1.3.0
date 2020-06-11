6cbfb8a9e5c2d2adf88ca4f4c1d85ab6f3cf41cb
alexxxx147 <alexxxx147@gmail.com>
2020-06-11 08:55:41 +0800

    簽入測試2

--- BCWinForm/Properties/AssemblyInfo.cs
+++ BCWinForm/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 指定為預設值:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.9.17")]
-[assembly: AssemblyFileVersion("1.9.18")]
+[assembly: AssemblyFileVersion("1.9.17")]
